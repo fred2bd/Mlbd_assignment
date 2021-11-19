ccccbdddf59d554c5ff86b4c53afe311b23da6f3
fahad <fred2bd@hotmail.com>
2021-11-19 22:10:28 +0600

    Navigation, MainActivity, Toolbar
    added navigation in MainAcitivity
    added custom toolbar with title textview and back button
    added FragmentContainerView in MainActivity  to host nav graph
    added progressbar to show loading

--- app/src/main/java/photos/picsum/mlbdproject/view/MainActivity.kt
+++ app/src/main/java/photos/picsum/mlbdproject/view/MainActivity.kt
@@ -2,11 +2,28 @@ package photos.picsum.mlbdproject.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import photos.picsum.mlbdproject.R
+import photos.picsum.mlbdproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.backBtn.setOnClickListener {
+            onBackPressed()
+
+
+        }
     }
+
 }
