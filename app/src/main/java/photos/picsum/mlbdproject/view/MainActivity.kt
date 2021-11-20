5dcd7b689d35b185609ec688f122a1f4a36a44e6
fahad <fred2bd@hotmail.com>
2021-11-20 23:37:07 +0600

    MainActivity, PictureListFragment, fragment_picture_list
    - Added shared viewModel callbacks to handle toast message and loading state
    - Added PictureListFragment and xml

--- app/src/main/java/photos/picsum/mlbdproject/view/MainActivity.kt
+++ app/src/main/java/photos/picsum/mlbdproject/view/MainActivity.kt
@@ -2,15 +2,25 @@ package photos.picsum.mlbdproject.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import kotlinx.coroutines.flow.collectLatest
 import photos.picsum.mlbdproject.R
 import photos.picsum.mlbdproject.databinding.ActivityMainBinding
+import photos.picsum.mlbdproject.view.picture_list.PictureListViewModel
+import photos.picsum.mlbdproject.view_model.SharedViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private val sharedViewModel: SharedViewModel by viewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +29,39 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         navController = navHostFragment.navController
+
         binding.backBtn.setOnClickListener {
             onBackPressed()
 
 
         }
+
+        lifecycleScope.launchWhenStarted {
+            sharedViewModel.data.collectLatest {
+
+                when (it) {
+                    is SharedViewModel.CommunicationState.Loading -> {
+
+                        if (it.show) {
+
+                            binding.progressBar.visibility = View.VISIBLE
+
+                        } else {
+                            binding.progressBar.visibility = View.GONE
+
+                        }
+
+                    }
+                    is SharedViewModel.CommunicationState.Msg -> {
+                        Toast.makeText(this@MainActivity, it.msg, Toast.LENGTH_LONG).show()
+                    }
+
+                }
+
+            }
+        }
+
+
     }
 
 }
---
+++ app/src/main/java/photos/picsum/mlbdproject/view/picture_list/PictureListFragment.kt
@@ -0,0 +1,23 @@
+package photos.picsum.mlbdproject.view.picture_list
+
+import android.os.Bundle
+import android.view.View
+import photos.picsum.mlbdproject.databinding.PictureListFragmentBinding
+import photos.picsum.mlbdproject.view.BaseFragment
+
+class PictureListFragment : BaseFragment<PictureListFragmentBinding>(PictureListFragmentBinding::inflate) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
